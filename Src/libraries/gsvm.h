152bec6195984084ed3d4d86fc03984dc91afa2e
MtjGD <ranieldees@gmail.com>
2024-10-20 14:32:23 +0000

    GeoScript version manager

---
+++ Src/libraries/gsvm.h
@@ -0,0 +1,14 @@
+void help() {}
+
+void version() {
+    printf("GeoScript Version Manager ( ver 1.0.0 ) [ x64 Windows ]\n");
+}
+
+void usage() {
+    printf("Usage: gsvm -s [version]\n");
+    printf("    gsvm --upgrade\n\n");
+
+    printf("    gsvm --version\n");
+    printf("    gsvm --usage");
+    printf("    gsvm --help");
+}
